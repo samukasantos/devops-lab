9f10cb4163de81442d9c01b2d63e9d35fe896d4d
Samuel Santos <samuelsantos18@gmail.com>
2023-08-23 01:56:18 +1000

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World"
-
-if __name__ == '__main__':
-    app.run()
